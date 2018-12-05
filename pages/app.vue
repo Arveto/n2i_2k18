7e3a0cdeffcf7ad8d10f914c7babaaf3eb66f0d8
TERRUSS <olivier.benaben@protonmail.com>
2018-12-05 22:26:29 +0100

    first implementation of a mobile app demo in browser

---
+++ pages/app.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="root">
+    <img src="@/static/phone.png" alt=""/>
+    <iframe src="http://51.75.92.38:8080" width="1080" height="1920"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+  img{
+    position: absolute;
+    top: 20px;
+    width: 600px;
+    height: 700px;
+  }
+
+  iframe{
+    position: absolute;
+    top: 95px;
+    left: 130px;
+
+    width: 340px;
+    height: 540px;
+  }
+</style>
