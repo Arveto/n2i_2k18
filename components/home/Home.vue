3d225771118bff61fc0ef67a41a673c39f305f8e
PurplePachyderm <a.delval87@gmail.com>
2018-12-07 02:38:06 +0100

    Start reskin

--- components/home/Home.vue
+++ components/home/Home.vue
@@ -17,45 +17,42 @@
         <div class="column">
             <span class="icon is-large">
                 <span class="fa-stack">
-                    <i class="fas fa-cogs fade-in"></i>
+                    <i class="fas fa-bolt fade-in"></i>
                 </span>
             </span>
-            <h3 class="title">Adaptable</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur.
+            <h3 class="title">Improvise</h3>
+            N'importe quel baroudeur vous le dira: la vie d'aventurier n'est pas toujours facile.
+            Heureusement pour vous, Arveto Softwares a dédié sa Nuit de l'Info 2018 a créer une application
+            qui rendra vos excursions bien plus sûres. Découvrez <i>A Gentleman's Guide to Wildlife Survival</i>,
+            déjà recommandé par les plus grands explorateurs du globe.
 
         </div>
 
         <div class="column">
             <span class="icon is-large">
                 <span class="fa-stack">
-                    <i class="fas fa-heart fade-in"></i>
+                    <i class="fas fa-campground fade-in"></i>
                 </span>
             </span>
-            <h3 class="title">User-targeted</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur.
+            <h3 class="title">Adapt</h3>
+            En compagnie d'Alberto, le premier poulpe a avoir effectué le tour du monde en solitaire (!), vous
+            trouverez dans ce site votre meilleur ami lorsque les vivres viennent à manquer, que les itempéries s'abattent
+            sur vous, et que le moral est au plus bas. Grâce à ses fonctionnalités révolutionnaires dans le monde de la survie,
+            vous aurez accès à une toute nouvelle manière de repousser les frontières de l'inconnu.
 
         </div>
 
         <div class="column">
             <span class="icon is-large">
                 <span class="fa-stack">
-                    <i class="fas fa-rocket fade-in"></i>
+                    <i class="fas fa-utensils fade-in"></i>
                 </span>
             </span>
-            <h3 class="title">Future proof</h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur.
+            <h3 class="title">Overcome</h3>
+            Adoptez <i>A Gentleman's Guide to Wildlife Survival</i>, et plus aucun sommet ne vous résistera, plus aucune
+            forêt reculée ne vous sera inaccessible, plus aucun désert ne sera infranchissable. Utilisée et approuvé par les
+            plus grands aventuriers des temps modernes, tels que Bear Grylls et Mike Horn, cette plate-forme promet de révolutionner
+            l'exploration.
         </div>
 
     </div>
